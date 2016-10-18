b7674761d8ef16ce010437cb0d8a4c9812ad8536
mohit-j <mohit_joshi@wireless-10-146-108-29.public.utexas.edu>
2016-10-17 20:27:48 -0500

    Created Critter4

---
+++ src/assignment4/Critter4.java
@@ -0,0 +1,24 @@
+package assignment4;
+
+public class Critter4 extends Critter {
+	
+	
+	@Override
+	public String toString(){
+		return "^";
+	}
+	
+	public Critter4() {
+		
+	}
+	
+	@Override
+	public void doTimeStep() {
+		
+	}
+	
+	@Override
+	public boolean fight(String oponent) {
+		
+	}
+}
