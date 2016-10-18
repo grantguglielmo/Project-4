da522e0a176f4ef0fd1bfb70f001bfc5d76a7828
mohit-j <mohit_joshi@wireless-10-146-108-29.public.utexas.edu>
2016-10-17 20:27:33 -0500

    Created Critter3

---
+++ src/assignment4/Critter3.java
@@ -0,0 +1,24 @@
+package assignment4;
+
+public class Critter3 extends Critter {
+	
+	
+	@Override
+	public String toString(){
+		return "#";
+	}
+	
+	public Critter3() {
+		
+	}
+	
+	@Override
+	public void doTimeStep() {
+		
+	}
+	
+	@Override
+	public boolean fight(String oponent) {
+		
+	}
+}
