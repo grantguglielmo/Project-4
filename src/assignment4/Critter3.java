af4576f6dd153fe37f20486960670d26a15ac2b8
mohit-j <mohit_joshi@wireless-10-146-108-29.public.utexas.edu>
2016-10-17 21:17:56 -0500

    Finished Critter3

--- src/assignment4/Critter3.java
+++ src/assignment4/Critter3.java
@@ -1,7 +1,30 @@
+/* CRITTERS Critter2.java
+ * EE422C Project 4 submission by
+ * Grant Guglielmo
+ * gg25488
+ * 16470
+ * Mohit Joshi
+ * msj696
+ * 16475
+ * Slip days used: 0
+ * Fall 2016
+ */
+
 package assignment4;
 
+/**
+ * This critter begins by randomly choosing a direction out of the possible eight. 
+ * If the direction it chooses will not result in backtracking, then it will walk in that
+ * direction. If the direction it chooses is the opposite of the last direction it walked,
+ * it will instead reproduce, with the offspring's direction set as the last direction walked.
+ * 
+ * If the critter reproduced during the current time step, it will fight to protect it's 
+ * offspring. Otherwise, it will choose not to fight.
+ */
 public class Critter3 extends Critter {
 	
+	private int prev;
+	private boolean babies;
 	
 	@Override
 	public String toString(){
@@ -9,16 +32,30 @@ public class Critter3 extends Critter {
 	}
 	
 	public Critter3() {
-		
+		prev = -1;
+		babies = false;
 	}
 	
 	@Override
 	public void doTimeStep() {
-		
+		int temp = Critter.getRandomInt(8);
+		if(temp == prev)			//check if movement will result in backtrack
+		{
+			Critter3 child = new Critter3();
+			this.reproduce(child, temp);
+			babies = true;
+			prev = -1;
+		}
+		else
+		{
+			walk(temp);
+			babies = false;
+			prev = (temp+4)%8;		//set prev to opposite of walking direction
+		}
 	}
 	
 	@Override
 	public boolean fight(String oponent) {
-		
+		return babies;				//fight if critter reproduced during this time step
 	}
 }
